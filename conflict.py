c47b64d109f74245d82187c9f2ca524c52078258
Neptunium6195 <seratesarah@gmail.com>
2026-09-18 14:10:54 -0700

    Update conflict.py

--- conflict.py
+++ conflict.py
@@ -1,1 +1,6 @@
+<<<<<<< Updated upstream
 print("Your sweater is strange")
+=======
+print("Your sweater is strange")
+#wowowow gfdfgiw sifehagrhiuedhfier t e erfhdIuh ergihergieghvbbhgrrrrrrrrrrrrrr. why is your sweater having an identity crisis. it cant be both a jacket and a sweater. such insecurity-
+>>>>>>> Stashed changes
